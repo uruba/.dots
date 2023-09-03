636c0562b3d7b8399002d6189ffe5ff2e45890a4
Václav Uruba <uruba@outlook.com>
2023-09-03 17:50:00 +0200

    respect the git root for telescope

--- nvim/.config/nvim/lua/lazy-plugins/telescope.lua
+++ nvim/.config/nvim/lua/lazy-plugins/telescope.lua
@@ -1,3 +1,18 @@
+function git_root()
+    return string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
+end
+
+function get_opts()
+    local opts = {}
+
+    local root = git_root()
+    if vim.v.shell_error == 0 then
+        opts.cwd = root
+    end
+
+    return opts
+end
+
 -- Telescope
 return {
     "nvim-telescope/telescope.nvim",
@@ -7,19 +22,27 @@ return {
     keys = {
         {
             "<leader>fp",
-            function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+            function()
+                require("telescope.builtin").find_files(get_opts())
+            end,
         },
         {
             "<leader>ff",
-            function() require('telescope.builtin').live_grep() end,
+            function()
+                require('telescope.builtin').live_grep(get_opts())
+            end,
         },
         {
             "<leader>fb",
-            function() require('telescope.builtin').buffers() end,
+            function()
+                require('telescope.builtin').buffers(get_opts())
+            end,
         },
         {
             "<leader>fh",
-            function() require('telescope.builtin').help_tags() end,
+            function()
+                require('telescope.builtin').help_tags(get_opts())
+            end,
         },
     },
 }
